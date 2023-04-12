845e8d11230617f2258e51a4118a185782cd3492
Nicholas <nicholas831@gmail.com>
2023-04-11 18:43:33 -0700

    git config setup and remove copy of full directory for no reason

--- install.sh
+++ install.sh
@@ -4,15 +4,11 @@ echo "INSTALLING Required Libraries..."
 
 sudo apt upgrade && sudo apt update
 sudo apt install thefuck htop net-tools 
-
+python3 -m pip install --upgrade pip
+python3 -m pip install python3-ensurepip
+python3 -m pip install -r requirements.txt
 
 echo "INSTALLING NPORC..."
-
-# Creating npo directory
-if [ ! -d ~/.npo ]; then
-  mkdir ~/.npo
-  cp -r . ~/.npo
-fi
 
 # Check if ~/.bashrc exists
 if [ -f ~/.bashrc ]; then
@@ -21,14 +17,14 @@ if [ -f ~/.bashrc ]; then
     if grep -q "#:begin" ~/.bashrc; then
         echo "updating .bashrc..."
         sed -i '/#:begin/,$d' ~/.bashrc
-        cat ~/.npo/.nporc | sed -n '/#:begin/,$p' >> ~/.bashrc
+        cat ~/.nporc/.nporc | sed -n '/#:begin/,$p' >> ~/.bashrc
     else
-        cat ~/.npo/.nporc >> ~/.bashrc
+        cat ~/.nporc/.nporc >> ~/.bashrc
     fi
 else
     echo "~/.bashrc does not exist, creating..."
     /bin/cp /etc/skel/.bashrc ~/
-    cat ~/.npo/.nporc >> ~/.bashrc
+    cat ~/.nporc/.nporc >> ~/.bashrc
 fi
 
 # Check if ~/.bash_aliases exists
@@ -38,18 +34,18 @@ if [ -f ~/.bash_aliases ]; then
     if grep -q "#:begin" ~/.bash_aliases; then
         echo "updating .bash_aliases..."
         sed -i '/#:begin/,$d' ~/.bash_aliases
-        cat ~/.npo/.npo_aliases | sed -n '/#:begin/,$p' >> ~/.bash_aliases
+        cat ~/.nporc/.npo_aliases | sed -n '/#:begin/,$p' >> ~/.bash_aliases
     else
-        cat ~/.npo/.npo_aliases >> ~/.bash_aliases
+        cat ~/.nporc/.npo_aliases >> ~/.bash_aliases
     fi
 else
     echo "~/.bash_aliases does not exist, creating..."
     touch ~/.bash_aliases
-    cat ~/.npo/.npo_aliases >> ~/.bash_aliases
+    cat ~/.nporc/.npo_aliases >> ~/.bash_aliases
 fi
 
 echo "DONE INSTALLING NPORC..."
-sleep 0.5
+sleep 1
 
 source ~/.bashrc
 
